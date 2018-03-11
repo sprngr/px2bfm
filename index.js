de4f69f1ff72702876eb08aa5da50dbeea359bc0
Michael Springer <mike@sprngr.com>
2018-03-11 03:09:37 -0400

    Finish out template files, update initial mapping of character

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const Jimp = require('jimp');
 
 const bitMaskMap = [1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768];
+
+// Currently supports first set of glyphs bitfontmaker2, extended set soon
 const glyphMapLayout = [
     [65,66,67,68,69,70,71,72,73,74,75,76,77],
-    [78,79,80,81,82,83,84,85,86,87,88,89,90]
+    [78,79,80,81,82,83,84,85,86,87,88,89,90],
+    [97,98,99,100,101,102,103,104,105,106,107,108,109],
+    [110,111,112,113,114,115,116,117,118,119,120,121,122],
+    [48,49,50,51,52,53,54,55,56,57,33,34,35],
+    [36,37,38,39,40,41,42,43,44,45,46,47,58],
+    [59,60,61,62,63,64,91,92,93,94,95,96,123],
+    [124,125,126,161,162,163,164,165,166,167,168,169,8364],
 ];
 const glyphSize = 16;
 
@@ -17,7 +25,7 @@ let glyphMapDimensions = {
 }
 let glyphMapOutput = {};
 
-Jimp.read('./templates/template.png')
+Jimp.read('./templates/test.png')
     .then((image) => {
         let height = image.bitmap.height;
         let width = image.bitmap.width;
@@ -74,20 +82,18 @@ function processPixels() {
 
             // if outer x will be out of bounds (13)
             // reset it to 0, bump the inner y
-            if (outerIndex.x == 13) {
+            if (outerIndex.x == glyphMapDimensions.width / glyphSize) {
                 outerIndex.x = 0;
                 innerIndex.y++;
 
                 // if inner y is out of bounds (16)
                 // reset it to 0, bump the outer y
-                if (innerIndex.y == 16) {
+                if (innerIndex.y == glyphSize) {
                     innerIndex.y = 0;
                     outerIndex.y++;
                 }
             }
         }
-
-        if (outerIndex.y == 2) return true;
 
         if (pixel.r == 0 && pixel.g == 0 && pixel.b == 0) {
             outputJSON[glyphMapLayout[outerIndex.y][outerIndex.x]][innerIndex.y] += bitMaskMap[innerIndex.x];
