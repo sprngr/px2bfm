a9a11e3cc9612ba34bc21b310ce1171623b8bfd5
Michael Springer <michael.springer@theresumator.com>
2018-03-13 00:11:08 -0400

    Added docs stub

--- docs/app.js
+++ docs/app.js
@@ -1,3 +1,57 @@
 'use strict';
 
+require('buffer');
 const px2bfm = require('../lib/px2bfm.js');
+
+// Drag and Drop code based off Smashing Magazine
+// https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
+let dropArea = document.getElementById('drop-area')
+
+dropArea.addEventListener('drop', handleDrop, false)
+
+function handleDrop(e) {
+  let dt = e.dataTransfer
+  let files = dt.files
+
+  handleFile(file);
+}
+
+function handleFile(files) {
+    previewFile(file);
+    console.log(file);
+}
+
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, preventDefaults, false)
+});
+
+['dragenter', 'dragover'].forEach(eventName => {
+  dropArea.addEventListener(eventName, highlight, false)
+});
+
+['dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, unhighlight, false)
+});
+
+function highlight(e) {
+  dropArea.classList.add('highlight')
+}
+
+function preventDefaults (e) {
+  e.preventDefault()
+  e.stopPropagation()
+}
+
+function previewFile(file) {
+  let reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onloadend = function() {
+    let img = document.createElement('img')
+    img.src = reader.result
+    document.getElementById('gallery').appendChild(img)
+  }
+}
+
+function unhighlight(e) {
+  dropArea.classList.remove('highlight')
+}
